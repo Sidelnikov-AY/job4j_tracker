a17ca41fbe8bf6dfcbea4e4c3ed5938ba30667a6
sid <1@1.ru>
2020-11-04 22:36:11 +0500

    1.5. Поля объекта [#391939]

--- src/main/java/oop/Cat.java
+++ src/main/java/oop/Cat.java
@@ -2,6 +2,22 @@ package oop;
 
 public class Cat {
 
+    private String food;
+    private String name;
+
+    public void givenNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may - may";
         return voice;
@@ -9,7 +25,13 @@ public class Cat {
 
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.givenNick("Gav");
+        gav.show();
+        Cat black = new Cat();
+        black.givenNick("Black");
+        black.eat("fish");
+        black.show();
     }
 }
