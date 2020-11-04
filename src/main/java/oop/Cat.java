6f5fd40c92372cdb70ae304231f671fb0ae0157d
sid <1@1.ru>
2020-11-04 22:20:11 +0500

    1.4. Вызов метода с возвращаемым типом. [#391943]

--- src/main/java/oop/Cat.java
+++ src/main/java/oop/Cat.java
@@ -1,6 +1,15 @@
 package oop;
 
 public class Cat {
-    Cat peppy = new Cat();
-    Cat sparky = new Cat();
+
+    public String sound() {
+        String voice = "may - may";
+        return voice;
+    }
+
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
+    }
 }
