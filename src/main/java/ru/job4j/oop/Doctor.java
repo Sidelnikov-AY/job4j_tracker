01ca7b81177646732a2e731df18ae8114940ff12
sid <1@1.ru>
2020-11-08 15:19:56 +0500

    3. Реализация профессий в коде [#391957]

---
+++ src/main/java/ru/job4j/oop/Doctor.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Doctor extends Profession{
+    private int specialization;
+
+    public Diagnosis heal(Pacient pacient) {
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Engineer.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Engineer extends Profession {
+    private String programLanguage;
+
+    public Program create (Program program) {
+    }
+
+}
