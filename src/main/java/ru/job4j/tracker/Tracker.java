1e8ebd158ee5b315931cf2cb4e9d27f1887d38b9
sid <1@1.ru>
2020-11-09 23:36:12 +0500

    6. Метод замены заявки. Tracker.replace [#391963]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -16,9 +16,9 @@ public class Tracker {
 
     public Item[] findAll() {
         Item[] itemsWithoutNull = new Item[size];
-        for(int index = 0; index < size; index++) {
+        for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if(item != null) {
+            if (item != null) {
                 itemsWithoutNull[index] = item;
             }
         }
@@ -39,14 +39,29 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        } else {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
     }
 }
