a5029ce9a2beda251e44da4617d797be6f7d34c6
sid <1@1.ru>
2020-11-22 11:37:19 +0500

    1.1. Тесты на StartUI. [#391912]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -155,4 +155,26 @@ public class StartUITest {
         ));
     }
 
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "7", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. === Exit Program ===%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. === Exit Program ===%n"
+                )
+        ));
+    }
+
 }
