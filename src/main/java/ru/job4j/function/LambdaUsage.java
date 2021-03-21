c48dd43cc574ae98be829234cd2e4ff126e876aa
sid <1@1.ru>
2021-03-21 14:33:20 +0500

    2.2. Лямбда блок [#391691]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,28 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        String[] text = {"one", "two", "three", "four"};
+        Comparator<String> cmpText = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right + " result: ");
+            return left.compareTo(right);
+        };
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+        Arrays.sort(text, cmpText);
+        for (String s:
+             text) {
+            System.out.println(s);
+        }
+        Arrays.sort(text, cmpDescSize);
+        for (String s:
+                text) {
+            System.out.println(s);
+        }
+    }
+}
