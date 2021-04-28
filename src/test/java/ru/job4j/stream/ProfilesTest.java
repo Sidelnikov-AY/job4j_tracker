09a2f985e93c60c09e757e19ec0d771547d140a2
asidelnikov <a@a.ru>
2021-04-28 12:19:44 +0500

    3. Уникальность элементов и сортировка. [#391701]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -1,26 +1,27 @@
 package ru.job4j.stream;
-
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
 
     @Test
-    public void WhenCollected() {
+    public void WhenCollectedAndDeleteCopies() {
         List<Profile> prof = List.of(
                 new Profile(new Address("moscow", "lubyanka", 21, 45)),
+                new Profile(new Address("moscow", "lubyanka", 21, 45)),
+                new Profile(new Address("moscow", "lubyanka", 22, 45)),
+                new Profile(new Address("chelyabinsk", "surovaya", 42, 666)),
                 new Profile(new Address("chelyabinsk", "surovaya", 42, 666))
         );
         Profiles ps = new Profiles();
         List<Address> rsl = ps.collect(prof);
         List<Address> expected = new ArrayList<>();
-        expected.add(new Address("moscow", "lubyanka", 21, 45));
         expected.add(new Address("chelyabinsk", "surovaya", 42, 666));
+        expected.add(new Address("moscow", "lubyanka", 21, 45));
+        expected.add(new Address("moscow", "lubyanka", 22, 45));
         assertThat(rsl, is(expected));
     }
 
@@ -32,5 +33,4 @@ public class ProfilesTest {
         List<Address> expected = new ArrayList<>();
         assertThat(rsl, is(expected));
     }
-
 }
