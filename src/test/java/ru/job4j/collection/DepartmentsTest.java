98389b0ceb6d27cb8cc62c997ba1d27b48b2fadb
asidelnikov <a@a.ru>
2021-04-30 22:49:45 +0500

    check Checkstyle3

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -33,13 +33,11 @@ public class DepartmentsTest {
         List<String> expect = Arrays.asList("k1",
                 "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2", "k2",
                 "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2");
-
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
-
     public void whenDescSorted() {
         List<String> input = Arrays.asList("k2/sk1/ssk1", "k2/sk1", "k2", "k2/sk1/ssk2",
                 "k1", "k1/sk2", "k1/sk1/ssk2", "k1/sk1/ssk1", "k1/sk1");
