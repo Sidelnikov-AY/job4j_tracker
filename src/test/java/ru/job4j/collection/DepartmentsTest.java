898c9a08964096e9cf4982065a6b2603c5fe9d3d
asidelnikov <a@a.ru>
2021-05-02 15:55:13 +0500

    1. Метод .of [#391710]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -13,7 +13,7 @@ public class DepartmentsTest {
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -21,7 +21,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -30,7 +30,7 @@ public class DepartmentsTest {
     public void whenAscSorted() {
         List<String> input = Arrays.asList("k2/sk1/ssk1", "k2/sk1", "k2", "k2/sk1/ssk2",
                 "k1", "k1/sk2", "k1/sk1/ssk2", "k1/sk1/ssk1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1",
+        List<String> expect = List.of("k1",
                 "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2", "k2",
                 "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2");
         List<String> result = Departments.fillGaps(input);
@@ -41,7 +41,7 @@ public class DepartmentsTest {
     public void whenDescSorted() {
         List<String> input = Arrays.asList("k2/sk1/ssk1", "k2/sk1", "k2", "k2/sk1/ssk2",
                 "k1", "k1/sk2", "k1/sk1/ssk2", "k1/sk1/ssk1", "k1/sk1");
-        List<String> expect = Arrays.asList("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1",
+        List<String> expect = List.of("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2", "k1",
                 "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2");
         Departments.sortDesc(input);
         assertThat(input, is(expect));
