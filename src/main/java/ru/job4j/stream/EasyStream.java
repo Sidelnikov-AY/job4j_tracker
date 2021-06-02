d95fcac4ea54c3ca917b793100889a89f621ed6b
asidelnikov <a@a.ru>
2021-06-02 15:54:56 +0500

    1. Экзамен. Lambda, Stream API, JDK Features. [#86058 #162721]

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -1,129 +1,44 @@
 package ru.job4j.stream;
-/*
-Представьте, что разработчики JDK не сделали Stream API.
 
-В этом задании Вам нужно реализовать каркас упрощенного Stream API.
-
-package ru.job4j.stream;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class EasyStream {
+    private final List<Integer> source;
+
+    public EasyStream(List<Integer> source) {
+        this.source = source;
+    }
+
     public static EasyStream of(List<Integer> source) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        List<Integer> list = new ArrayList<>();
+        for (Integer el : source) {
+            list.add(fun.apply(el));
+        }
+        return new EasyStream(list);
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        List<Integer> list = new ArrayList<>();
+        for (Integer el : source) {
+            if (fun.test(el)) {
+                list.add(el);
+            }
+        }
+        return new EasyStream(list);
     }
 
     public List<Integer> collect() {
-        throw new UnsupportedOperationException();
-    }
-}
-EasyStream работает только с типом Integer. В нем есть четыре метода
-
-of - получает исходные данные.
-
-map - преобразует число в другое число.
-
-filter - фильтрует поток элементов.
-
-collect - собирает все элементы из source по заданным условиям map и filter.
-
-package ru.job4j.stream;
-
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-import org.junit.Test;
-import java.util.List;
-
-public class EasyStreamTest {
-
-    @Test
-    public void whenEmptySource() {
-        var rsl = EasyStream.of(List.of()).collect();
-        assertThat(rsl, is(List.of()));
-    }
-
-    @Test
-    public void whenFilledSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3))
-                .collect();
-        assertThat(rsl, is(List.of(1, 2, 3)));
-    }
-
-    @Test
-    public void whenFilteredSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3))
-                .filter(e -> e == 2)
-                .collect();
-        assertThat(rsl, is(List.of(2)));
-    }
-
-    @Test
-    public void whenMappedSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3))
-                .map(e -> e * 2)
-                .collect();
-        assertThat(rsl, is(List.of(2, 4, 6)));
-    }
-
-    @Test
-    public void whenFilteredMappedSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3))
-                .filter(e -> e == 2)
-                .map(e -> e * 2)
-                .collect();
-        assertThat(rsl, is(List.of(4)));
-    }
-
-    @Test
-    public void whenMappedFilteredSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3))
-                .map(e -> e * 2)
-                .filter(e -> e == 2)
-                .collect();
-        assertThat(rsl, is(List.of(2)));
-    }
-
-    @Test
-    public void whenMultyFilterSource() {
-        var rsl = EasyStream.of(List.of(1, 2, 3, 4, 5))
-                .filter(e -> e >= 2)
-                .filter(e -> e <= 4)
-                .collect();
-        assertThat(rsl, is(List.of(2, 3, 4)));
-    }
-}
-В этом задании нужно использовать шаблон Builder.
-* */
-
-
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
-public class EasyStream {
-    public static EasyStream of(List<Integer> source) {
-        throw new UnsupportedOperationException();
-    }
-
-    public EasyStream map(Function<Integer, Integer> fun) {
-        throw new UnsupportedOperationException();
-    }
-
-    public EasyStream filter(Predicate<Integer> fun) {
-        throw new UnsupportedOperationException();
-    }
-
-    public List<Integer> collect() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        return new ArrayList<>(source);
     }
 }
